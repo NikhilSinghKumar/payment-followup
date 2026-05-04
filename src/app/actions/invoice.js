b8b5b87f08c86cfb868d66e8a1fa06ea840a1873
Nikhil Kumar <nikhilsk369@gmail.com>
2026-05-04 14:16:13 +0530

    adding search

--- src/app/actions/invoice.js
+++ src/app/actions/invoice.js
@@ -2,25 +2,39 @@
 
 import { db } from "@/db";
 import { invoices, clients, payments } from "@/db/schema";
-import { eq, sql } from "drizzle-orm";
+import { eq, sql, ilike, or } from "drizzle-orm";
 
-export async function getInvoices() {
-  const data = await db
+export async function getInvoices(search) {
+  let query = db
     .select({
       id: invoices.id,
       amount: invoices.amount,
       dueDate: invoices.dueDate,
       companyName: clients.companyName,
 
-      // 🔥 total paid
+      // total paid
       paid: sql`COALESCE(SUM(${payments.amount}), 0)`,
     })
     .from(invoices)
     .leftJoin(clients, eq(invoices.clientId, clients.id))
-    .leftJoin(payments, eq(payments.invoiceId, invoices.id))
-    .groupBy(invoices.id, clients.companyName);
+    .leftJoin(payments, eq(payments.invoiceId, invoices.id));
 
-  // 🔥 compute due + status
+  // Search
+
+  if (search) {
+    query = query.where(
+      or(
+        ilike(clients.companyCode, `%${search}%`),
+        ilike(clients.companyName, `%${search}%`),
+      ),
+    );
+  }
+
+  const data = await query
+    .groupBy(invoices.id, clients.companyName, clients.companyCode)
+    .orderBy(invoices.id);
+
+  // compute due + status
   return data.map((inv) => {
     const amount = Number(inv.amount);
     const paid = Number(inv.paid);
