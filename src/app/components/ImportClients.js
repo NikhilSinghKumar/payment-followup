b633f4295409829b00d9ef8f0e909ddbdd0e3954
Nikhil Kumar <nikhilsk369@gmail.com>
2026-06-18 12:36:10 +0530

    name change

--- src/app/components/ImportClients.js
+++ src/app/components/ImportClients.js
@@ -64,7 +64,7 @@ export default function ImportClients() {
             Importing
           </span>
         ) : (
-          "Import"
+          "Import Clients"
         )}
       </button>
     </div>
