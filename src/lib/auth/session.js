469ba704aa8add797152f9c702d3909aece9c99a
Nikhil Kumar <nikhilsk369@gmail.com>
2026-07-10 10:43:15 +0530

    session

---
+++ src/lib/auth/session.js
@@ -0,0 +1,100 @@
+import crypto from "crypto";
+
+import { db } from "@/db";
+import { sessions } from "@/db/schema";
+import { and, eq } from "drizzle-orm";
+
+const SESSION_TOKEN_BYTES = 32;
+const SESSION_DURATION_DAYS = 7;
+
+const INVALID_SESSION = {
+  session: null,
+  user: null,
+};
+
+export function generateSessionToken() {
+  return crypto.randomBytes(SESSION_TOKEN_BYTES).toString("hex");
+}
+
+export function hashSessionToken(token) {
+  return crypto.createHash("sha256").update(token).digest("hex");
+}
+
+export async function createSession({
+  userId,
+  ipAddress = null,
+  userAgent = null,
+}) {
+  // Generate session token
+  const sessionToken = generateSessionToken();
+
+  // Hash token before storing
+  const tokenHash = hashSessionToken(sessionToken);
+
+  // Session expiry
+  const expiresAt = new Date(
+    Date.now() + SESSION_DURATION_DAYS * 24 * 60 * 60 * 1000,
+  );
+
+  // Save session
+  await db.insert(sessions).values({
+    userId,
+    tokenHash,
+    ipAddress,
+    userAgent,
+    expiresAt,
+  });
+
+  return {
+    sessionToken,
+    expiresAt,
+  };
+}
+
+export async function validateSession(sessionToken) {
+  if (!sessionToken) {
+    return INVALID_SESSION;
+  }
+
+  const tokenHash = hashSessionToken(sessionToken);
+
+  const session = await db.query.sessions.findFirst({
+    where: eq(sessions.tokenHash, tokenHash),
+
+    with: {
+      user: true,
+    },
+  });
+
+  if (session.user.deletedAt) {
+    return INVALID_SESSION;
+  }
+
+  // Session not found
+  if (!session) {
+    return INVALID_SESSION;
+  }
+
+  // Session revoked
+  if (session.isRevoked) {
+    return INVALID_SESSION;
+  }
+
+  // Session expired
+  if (session.expiresAt < new Date()) {
+    await db.delete(sessions).where(eq(sessions.id, session.id));
+
+    return INVALID_SESSION;
+  }
+
+  // User inactive
+  if (!session.user.isActive) {
+    return INVALID_SESSION;
+  }
+
+  return {
+    session,
+    user: session.user,
+    companyId: null,
+  };
+}
